f0031e6a7fb466a8f482110cd1e5748706cc9924
McBen <viertelvor12@gmx.net>
2022-12-12 16:33:47 +0100

    plugin: Add highlight Inactive Portals

---
+++ core/code/plugin/highlighter/forgotten.ts
@@ -0,0 +1,40 @@
+import { DAYS } from "../../helper/times";
+import { IITC } from "../../IITC";
+import { Plugin } from "../plugin_base";
+
+
+export class PluginHighlightInactive extends Plugin {
+
+    public name = "Highlight inactive portals";
+    public version = "0.2.0";
+    public description =
+        "Use the portal fill color to denote if the portal is unclaimed with no recent activity." +
+        "Shades of red from one week to one month, then tinted to purple for longer." +
+        "May also highlight captured portals that are stuck and fail to decay every 24 hours.";
+    public author = "jonatkins";
+    public tags: ["portal", "highlight", "inactive", "unclaimed"];
+    public defaultInactive = true;
+    private menuName = "Inactive Portals";
+
+
+    inactivePortals = (portal: IITC.Portal): void => {
+        if (portal.options.timestamp > 0) {
+            const daysUnmodified = (Date.now() - portal.options.timestamp) / DAYS;
+            if (daysUnmodified >= 7) {
+                const fill_opacity = Math.min(1, ((daysUnmodified - 7) / 24) * .85 + .15);
+                const blue = Math.max(0, Math.min(255, Math.round((daysUnmodified - 31) / 62 * 255)));
+                const colour = `rgb(255,0,${blue})`;
+                const style = { fillColor: colour, fillOpacity: fill_opacity };
+                portal.setStyle(style);
+            }
+        }
+    }
+
+    activate(): void {
+        IITC.highlighter.add({ name: this.menuName, highlight: this.inactivePortals });
+    }
+
+    deactivate(): void {
+        IITC.highlighter.remove(this.menuName);
+    }
+}
