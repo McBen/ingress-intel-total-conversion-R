341c09958f802112b812ecb6b350be33a278592b
McBen <viertelvor12@gmx.net>
2022-12-29 19:20:28 +0000

    Deploying to gh-pages from @ McBen/ingress-intel-total-conversion-R@74e77211cc0cdc06b8d0a1bc82201e916667c0b8 🚀

--- nightly/total-conversion-build.meta.js
+++ nightly/total-conversion-build.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name            IITC: Ingress intel map total conversion R
 // @id              ingress-intel-total-conversion@McBen
-// @version         1.0.0.20221229.142130
+// @version         1.0.0.20221229.192027
 // @updateURL       https://mcben.github.io/ingress-intel-total-conversion-R/nightly/total-conversion-build.meta.js
 // @downloadURL     https://mcben.github.io/ingress-intel-total-conversion-R/nightly/total-conversion-build.user.js
-// @description     [2022-12-29-142129-dev] Total conversion for the ingress intel map - Refurbished.
+// @description     [2022-12-29-192026-dev] Total conversion for the ingress intel map - Refurbished.
 // @match           https://intel.ingress.com/*
 // @match           https://intel-x.ingress.com/*
 // @grant           none
