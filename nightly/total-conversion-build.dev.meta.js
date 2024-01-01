dde8137504ae52a37ff61c1adb75fa2a95c673b9
McBen <viertelvor12@gmx.net>
2024-01-01 18:58:17 +0000

    Deploying to gh-pages from @ McBen/ingress-intel-total-conversion-R@0adbb4d06ca0677458c6af330db8565ff8bbe3d3 🚀

---
+++ nightly/total-conversion-build.dev.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            IITC: Ingress intel map total conversion R
+// @id              ingress-intel-total-conversion@McBen
+// @version         1.0.0.20240101.185863
+// @updateURL       https://mcben.github.io/ingress-intel-total-conversion-R/nightly/total-conversion-build.user.js/total-conversion-build.dev.meta.js
+// @downloadURL     https://mcben.github.io/ingress-intel-total-conversion-R/nightly/total-conversion-build.user.js/total-conversion-build.dev.user.js
+// @description     [2024-01-01-185862-dev] Total conversion for the ingress intel map - Refurbished.
+// @match           https://intel.ingress.com/*
+// @match           https://intel-x.ingress.com/*
+// @grant           none
+// @run-at          document-end
+// ==/UserScript==
