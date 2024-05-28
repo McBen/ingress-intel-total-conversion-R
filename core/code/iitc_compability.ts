8586e7b0ac8a16bb7511fd0b923db5c2fda770c2
McBen <viertelvor12@gmx.net>
2024-05-28 19:45:10 +0200

    compatibilty: add chat dummies

--- core/code/iitc_compability.ts
+++ core/code/iitc_compability.ts
@@ -188,6 +188,11 @@ globalThis.iitcCompabilityInit = () => {
     }
 }
 
+globalThis.chat = {
+    postMsg: NOOP,
+    backgroundChannelData: NOOP,
+    getActive: () => { return "all" }
+};
 
 // IITC-CE v.0.38.0
 interface IITC_ButtonOptions {
