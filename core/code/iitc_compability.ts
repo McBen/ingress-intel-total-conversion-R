8ffad29adb853ae42b838b68a33a63cdb34da5ae
McBen <viertelvor12@gmx.net>
2024-05-29 15:27:20 +0200

    more compatibility
    # Conflicts:
    #	core/code/iitc_compability.ts

--- core/code/iitc_compability.ts
+++ core/code/iitc_compability.ts
@@ -186,6 +186,11 @@ globalThis.iitcCompabilityInit = () => {
     }
 }
 
+globalThis.chat = {
+    postMsg: NOOP,
+    backgroundChannelData: NOOP,
+    getActive: () => { return "all" }
+};
 
 // IITC-CE v.0.38.0
 interface IITC_ButtonOptions {
