977a3e2b46261d5c74d9141acbf980bd02baef27
McBen <viertelvor12@gmx.net>
2024-09-07 20:06:34 +0200

    add fav icon

--- core/total-conversion-build.js
+++ core/total-conversion-build.js
@@ -74,6 +74,8 @@ window.addEventListener(
 // possible without requiring scripts.
 document.head.innerHTML = ''
   + '<title>Ingress Intel Map</title>'
+  + '<meta charset="UTF-8">'
+  + '<link rel="shortcut icon" href="/img/favicon.ico" />'
   + '<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic&subset=latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic"/>';
 
 // remove body element entirely to remove event listeners
