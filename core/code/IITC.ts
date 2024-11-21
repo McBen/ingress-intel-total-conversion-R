ea7a7face93a3b6df3aae62b1a4d033c30110154
McBen <viertelvor12@gmx.net>
2024-11-21 08:32:17 +0100

    fields: move to extra folder

--- core/code/IITC.ts
+++ core/code/IITC.ts
@@ -14,68 +14,8 @@ import { Highlighters } from "./portal/highlighters";
 import { Artifacts } from "./artifact";
 import { setupSidebar } from "./ui/sidebar";
 import { setupLogWindow } from "./ui/log";
+import { Fields } from "./data/Fields";
 const log = Log(LogApp.Main);
-
-
-// TODO: FOR COMPATIBILITY -> move to iitc-comp layer
-Object.defineProperty(window, "fields", {
-    get: (): any => {
-        console.warn("window.fields getter has bad performace. Better use IITCr.fields.get(guid)");
-        // console.trace("window.fields getter");
-        return IITCr.fields.toOldObject();
-    },
-});
-
-class Fields {
-    public all: IITC.Field[];
-
-    constructor() {
-        this.all = [];
-    }
-
-    get(guid: FieldGUID): IITC.Field | undefined {
-        return this.all.find(f => f.options.guid === guid);
-    }
-
-
-    add(field: IITC.Field) {
-        this.all.push(field);
-    }
-
-
-    remove(guid: FieldGUID): IITC.Field | undefined {
-        const index = this.all.findIndex(f => f.options.guid === guid);
-        if (index >= 0) {
-            return this.all.splice(index, 1)[0];
-        }
-        return;
-    }
-
-
-    getByPortal(guid: PortalGUID): IITC.Field[] {
-        return this.all.filter(f => {
-            const d = f.options.data;
-            return d.points[0].guid === guid || d.points[1].guid === guid || d.points[2].guid === guid;
-        })
-    }
-
-    /**
-     * Fields with atleast one Vertex in area
-     */
-    getInBounds(bounds: L.LatLngBounds): IITC.Field[] {
-        return this.all.filter(field => {
-            const points = field.getLatLngs();
-            return bounds.contains(points[0]) || bounds.contains(points[1]) || bounds.contains(points[2]);
-        })
-    }
-
-    toOldObject(): Record<string, IITC.Field> {
-        const result: Record<string, IITC.Field> = {};
-        this.all.forEach(f => result[f.options.guid] = f);
-        return result;
-    }
-}
-
 
 export class IITCMain {
     readonly plugins: PluginManager;
